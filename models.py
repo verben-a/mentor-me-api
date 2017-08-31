08248c11c7f94f28c1ca91d8ec09a779f533207a
verben-a <alina.verbenchuk@gmail.com>
2017-08-31 22:57:10 +0100

    phase one

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from flask_login import UserMixin
 
 
 class User(Base):
+	"""ONE ----> to """
 	"""docstring for User"""
 	__tablename__ = "users"
 	id = Column(Integer, primary_key=True)
@@ -19,18 +20,21 @@ class Role(Base):
 	__tablename__= "roles"
 	id = Column(Integer, primary_key=True)
 	roletype = Column(String(100), default = "mentee")
+	""".... MANY """
 	user_id = Column(Integer, ForeignKey('users.id'), nullable = False)
 
 class Profile(Base):
-	"""docstring for Profile"""
+	"""docstring for Profile
+
+	ONE -_ > relationship"""
 	__tablename__ = "profiles"
 
 	id = Column(Integer, primary_key=True)
 	name_surname = Column(String(100))
 	summary = Column(String(128))
-	position = Column(String(100))
-	experience = Column(String(300))
-	education = Column(String(100))
+	position = Column(String(300)) 
+	experience = relationship("Experience", backref="experiences")
+	education = relationship("Education", backref="educations") 
 	location = Column(String(100))
 	languages = Column(String(100))
 	skills = Column(String(300))
@@ -38,7 +42,65 @@ class Profile(Base):
 	linkedin = Column(String(300))
 	facebook = Column(String(300))
 
-	user_id =Column(Integer, ForeignKey('users.id'), nullable = False)
+	user_id = Column(Integer, ForeignKey('users.id'), nullable = False)
+		
+
+class Experience(Base):
+	"""docstring for Position"""
+	__tablename__= "experiences"
+
+	id = Column(Integer, primary_key=True)
+	company_name = Column(String(100))
+	position_name = Column(String(100))
+	position_summary = Column(String(300))
+
+	profile_id = Column(Integer, ForeignKey("experiences.id"), nullable=False)
+
+class Education(Base):
+	"""docstring for education"""
+	__tablename__= "educations"
+
+	id = Column(Integer, primary_key=True)
+	university_name = Column(String(300))
+	major_name = Column(String(300))
+	education_summary = Column(String(400))
+
+	profile_id = Column(Integer, ForeignKey("educations.id"), nullable=False)
+
+class Language(Base):
+	"""docstring for Language"""
+	__tablename__="languages"
+
+	id = Column(Integer, primary_key=True)
+	language_name = Column(String(50))
+
+	profile_id = Column(Integer, ForeignKey("languages.id"), nullable=False)
+
+class Skill(Base):
+	"""docstring for Skill"""
+	__tablename__="skills"
+
+	id = Column(Integer, primary_key=True)
+	skill_name = Column(String(300))
+
+	profile_id = Column(Integer, ForeignKey("skills.id"), nullable=False)
+
+	
 		
 
 
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+		
